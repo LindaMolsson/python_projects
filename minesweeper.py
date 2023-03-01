3d2fed8d04024553bf8726975d8e17b6fce04e4f
LindaMolsson <125501652+LindaMolsson@users.noreply.github.com>
2023-03-01 19:01:50 +0100

    Restart added

--- minesweeper.py
+++ minesweeper.py
@@ -1,6 +1,7 @@
-import customtkinter
-import tkinter
-import random
+import customtkinter #GUI
+import random #Creat mines
+import os #restart
+import sys  #restart
 #from https://github.com/TomSchimansky/CustomTkinter/blob/master/examples/simple_example.py
 customtkinter.set_appearance_mode("dark")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("dark-blue")  # Themes: "blue" (standard), "green", "dark-blue"
@@ -9,7 +10,6 @@ app = customtkinter.CTk()
 app.title("Minesweeper")
 app.resizable(False, False)
 
-
 #function to get GRID size from user
 def grid_size():
     size_question = customtkinter.CTkInputDialog(text = "type in small, medium or large: ")
@@ -17,9 +17,9 @@ def grid_size():
     if size.lower() == "small":
         r = 5
         c = 8
-        n = 6
-        geo_x = 390
-        geo_y = 400
+        n = 9
+        geo_x = 470
+        geo_y = 500
     elif size.lower() == "medium":
         r = 10
         c = 12
@@ -30,8 +30,8 @@ def grid_size():
         r = 15
         c = 16
         n = 20
-        geo_x = 640
-        geo_y = 630
+        geo_x = 700
+        geo_y = 650
     return r, c, n, geo_x, geo_y
 
 size = grid_size()
@@ -48,7 +48,7 @@ frame_1 = customtkinter.CTkFrame(master = app)
 frame_1.pack(pady = 20, padx = 60, fill = "both", expand = True)
 frame_2 = customtkinter.CTkFrame(app, width = geo_x, height = geo_y)
 frame_2.place(x = 100, y = 80)
-frame_3 = customtkinter.CTkFrame(app, width = geo_x * 0.7, height = geo_y * 0.7)
+frame_3 = customtkinter.CTkFrame(app, width = geo_x * 0.7, height = geo_y * 0.8,corner_radius = 0)
 
 class Cell():
     all = []
@@ -110,7 +110,7 @@ class Cell():
             Cell.cell_count_object.configure(text = "Cells left: {}".format(Cell.cell_counted))
         self.is_open = True
 
-    #counts the amount of mines that
+    #gathers information about the selected cell's surrounding cells (using get_cell and count_mines)
     @property
     def get_surronded_cells(self):
         surronded_cells = [
@@ -126,11 +126,12 @@ class Cell():
         surronded_cells = [cell for cell in surronded_cells if cell is not None]
         return surronded_cells
     
+    #The function get_surronded_cells uses it to find surrounding cells
     def get_cell(self, x, y):
         for cell in Cell.all:
             if cell.x == x and cell.y == y:
                 return cell
-    
+    #the function show cell() uses it to determine how many mines that surrounds the cell (aka. what number to print on button)
     def count_mines(self):
         counter = 0
         for cell in self.get_surronded_cells:
@@ -138,13 +139,21 @@ class Cell():
                 counter += 1
         return counter
     
+    #if chosen cell.mine = True, this happens
     def show_mine(self):
         self.button_object.configure(fg_color = "red", hover_color = 'red')
         frame_3.place(x = 80, y = 80)
         game_lost = customtkinter.CTkLabel(frame_3, text = "BOOOM")
         game_lost.place(x = geo_x * 0.3, y = geo_y * 0.2)
+        Cell.show_reset_button()
+
+
+    @staticmethod
+    def show_reset_button():
+        button_restart = customtkinter.CTkButton(frame_3, border_width = 2,width = 60, height = 32, corner_radius = 1, text = "Play again", command= restart_program)
+        button_restart.place(x = geo_x * 0.3, y = geo_y * 0.3)
     
-        
+    #function to "mark" cells as mines.
     def right_click_button(self, event):
         self.button_object.configure(fg_color = "green", hover_color = 'green')
         Cell.mine_counted -= 1
@@ -159,22 +168,32 @@ class Cell():
             frame_3.place(x = 80, y = 80)
             game_won = customtkinter.CTkLabel(frame_3, text = "You won!")
             game_won.place(x = geo_x * 0.3, y = geo_y * 0.2)
+            Cell.show_reset_button()
 
     def __repr__(self):
         return "cell({y}, {x}).".format(y = self.y, x = self.x)
 
 
     # stolen from https://www.youtube.com/watch?v=OqbGRZx4xUc
-for x in range(r):
-    for y in range(c):
-        c1 = Cell(x, y)
-        c1.create_button()
-        c1.button_object.grid(column = x, row = y)
+#Creates the cells
+def create_cells():
+    for x in range(r):
+        for y in range(c):
+            c1 = Cell(x, y)
+            c1.create_button()
+            c1.button_object.grid(column = x, row = y)
 
+def restart_program():
+    python = sys.executable
+    os.execl(python, python, * sys.argv)
+
+
+create_cells()
 Cell.cell_count()
-Cell.cell_count_object.place(x = 0, y = 0)
+Cell.cell_count_object.place(x = 40, y = 0)
 Cell.mines_left()
-Cell.mine_count_object.place(x = 0, y = 20)
-
+Cell.mine_count_object.place(x = 40, y = 20)
 Cell.create_mines()
+
+
 app.mainloop()
